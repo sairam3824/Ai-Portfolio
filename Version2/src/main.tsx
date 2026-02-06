20f61b5387085b420ea7d22bafbddd64c400637e
MARURI SAI RAMA LINGA REDDY <Msrlreddy@outlook.com>
2026-02-06 09:25:13 +0530

    feat: update app entry and html structure

--- Version2/src/main.tsx
+++ Version2/src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+import reportWebVitals, { sendToAnalytics } from './shared/reportWebVitals'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
 )
+
+reportWebVitals(sendToAnalytics)
