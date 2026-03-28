dc8153d2e1f2516fb50d69120a7af1e03c53067c
MARURI SAI RAMA LINGA REDDY <Msrlreddy@outlook.com>
2026-03-28 09:01:50 +0530

    modified:   Version3/vite.config.ts

--- Version3/vite.config.ts
+++ Version3/vite.config.ts
@@ -59,6 +59,7 @@ function injectSiteMetadataPlugin(): Plugin {
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/v3/',
     plugins: [
         react(),
         preloadAvatarPlugin(),
