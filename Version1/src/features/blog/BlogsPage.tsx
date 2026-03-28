73324668f94c7e44ae5f8015ecfb9a912acc5014
MARURI SAI RAMA LINGA REDDY <Msrlreddy@outlook.com>
2026-03-28 08:52:50 +0530

    update blogs listing

--- Version1/src/features/blog/BlogsPage.tsx
+++ Version1/src/features/blog/BlogsPage.tsx
@@ -3,7 +3,7 @@ import Layout from "@/shared/components/Layout";
 
 const BlogsPage = () => {
   return (
-    <Layout title="Blogs">
+    <Layout>
       <div className="max-w-4xl mx-auto">
         <BlogSection />
       </div>
