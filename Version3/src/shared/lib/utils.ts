44e414f76ccbfc9b1ad0bbb8b2e30787c9511240
MARURI SAI RAMA LINGA REDDY <Msrlreddy@outlook.com>
2026-03-31 11:06:01 +0530

    update utils

--- Version3/src/shared/lib/utils.ts
+++ Version3/src/shared/lib/utils.ts
@@ -1,3 +1,5 @@
-export function cn(...inputs: any[]) {
+type ClassValue = string | false | null | undefined;
+
+export function cn(...inputs: ClassValue[]) {
     return inputs.filter(Boolean).join(" ");
 }
